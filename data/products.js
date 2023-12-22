095dce2fa302f8360ddd9b583ea74ef64fc7f365
dorcas odetayo <odetayodorcas123@gmail.com>
2023-12-22 17:33:37 +0100

    deleted unused codes and comments

--- data/products.js
+++ data/products.js
@@ -1,5 +1,20 @@
 const products = [
   {
+    id: "e454des5-6aa0-4b85-b27f-e1d07eb678c6",
+    image: "images/products/fanta.jpg",
+    name: "European Fanta Orange Soda 6-Pack of Cans 6 x 330 ml",
+    rating: {
+      stars: 5,
+      count: 95
+    },
+    priceCents: 1090,
+    keywords: [
+      "socks",
+      "sports",
+      "apparel"
+    ]
+  },
+  {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     image: "images/products/athletic-cotton-socks-6-pairs.jpg",
     name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
