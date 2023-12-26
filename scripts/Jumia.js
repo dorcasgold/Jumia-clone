3149830c9d211559df47d4da54ca7cd85bbe71ef
dorcas odetayo <odetayodorcas123@gmail.com>
2023-12-26 07:35:34 +0100

    fixed quantity-selector

--- scripts/Jumia.js
+++ scripts/Jumia.js
@@ -23,7 +23,7 @@ products.forEach((product) => {
         </div>
 
         <div class="product-quantity-container">
-          <select>
+          <select   class="js-quantity-selector-${product.id}">
             <option selected value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
@@ -64,13 +64,19 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) => {
       }
     });
 
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+    quantity = quantitySelector.value
+    quantity = Number(quantity)
+    console.log(quantity);
     //2
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1
+        quantity: quantity
       });
     }
     let cartquantity = 0
